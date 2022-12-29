b802d2eb822410ab2a25e5352d375dc0e46ebac0
Diney <dineydm@gmail.com>
2022-12-29 18:24:36 -0300

    move HttpAdapter to its own file

---
+++ lib/infra/http/http_adapter.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:meta/meta.dart';
+
+import '../../data/http/http.dart';
+
+class HttpAdapter implements HttpClient {
+  final Client client;
+
+  HttpAdapter(this.client);
+
+  Future<Map> request({
+    @required String url,
+    @required String method,
+    Map body,
+  }) async {
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json'
+    };
+
+    final jsonBody = body != null ? jsonEncode(body) : null;
+    final respose = await client.post(url, headers: headers, body: jsonBody);
+
+    if (respose.statusCode == 200) {
+      return respose.body.isEmpty ? null : jsonDecode(respose.body);
+    } else {
+      return null;
+    }
+  }
+}
