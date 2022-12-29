1666ce4380bfe017c70bce331009dc2e16c56808
Diney <dineydm@gmail.com>
2022-12-29 18:00:50 -0300

    ensure HttpAdapter returns null if post returns 200 with no data

--- lib/test/infra/http/http_adapter_test.dart
+++ lib/test/infra/http/http_adapter_test.dart
@@ -26,7 +26,7 @@ class HttpAdapter implements HttpClient {
     final jsonBody = body != null ? jsonEncode(body) : null;
     final respose = await client.post(url, headers: headers, body: jsonBody);
 
-    return jsonDecode(respose.body);
+    return respose.body.isEmpty ? null : jsonDecode(respose.body);
   }
 }
 
@@ -89,6 +89,15 @@ void main() {
       final response = await sut.request(url: url, method: 'post');
 
       expect(response, {'any_key': 'any_value'});
+    });
+
+    test('Should return null if post returns 200 with no data', () async {
+      when(client.post(any, headers: anyNamed('headers')))
+          .thenAnswer((_) async => Response('', 200));
+
+      final response = await sut.request(url: url, method: 'post');
+
+      expect(response, null);
     });
   });
 }
