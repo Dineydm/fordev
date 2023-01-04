13fa5c1e0e320c61a72dcded583fa0f70743c25d
Diney <dineydm@gmail.com>
2023-01-04 18:37:54 -0300

    move EmailValidation to its own file

---
+++ lib/validation/validators/email_validation.dart
@@ -0,0 +1,16 @@
+import '../../domain/helpers/helpers.dart';
+import '../protocols/protocols.dart';
+
+class EmailValidation implements FieldValidation {
+  final String field;
+
+  EmailValidation(this.field);
+
+  String validate(String value) {
+    final regex = RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    final isValid = value?.isNotEmpty != true || regex.hasMatch(value);
+
+    return isValid ? null : FieldError.invalidField.description;
+  }
+}
