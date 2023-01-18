090b70b5a5b72ccba2f650aa338c33d11fa4c535
Diney <dineydm@gmail.com>
2023-01-18 10:50:33 -0300

    ensure RemoteAddAccount thows EmailInUseError on 403

--- test/data/usecases/add_account/remote_add_account_test.dart
+++ test/data/usecases/add_account/remote_add_account_test.dart
@@ -75,4 +75,13 @@ void main() {
 
     expect(future, throwsA(DomainError.unexpected));
   });
+
+  test('Should thow InvalidCredentialsError if HttpClient returns 403',
+      () async {
+    mockHttpError(HttpError.forbidden);
+
+    final future = sut.add(params);
+
+    expect(future, throwsA(DomainError.emailInUse));
+  });
 }
