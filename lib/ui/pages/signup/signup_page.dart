18a444ad1306f89205501a96941ec8a8e9218464
Sandro <valksandro@gmail.com>
2023-01-19 10:29:37 -0300

    ensure SignUpPage calls validation on any field change

--- lib/ui/pages/signup/signup_page.dart
+++ lib/ui/pages/signup/signup_page.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../components/components.dart';
 import '../../helpers/helpers.dart';
 import './components/components.dart';
+import './signup_presenter.dart';
 
 class SignUpPage extends StatelessWidget {
+  final SignUpPresenter presenter;
+
+  SignUpPage(this.presenter);
+
   @override
   Widget build(BuildContext context) {
     void _hideKeyboard() {
@@ -28,27 +34,31 @@ class SignUpPage extends StatelessWidget {
                   Headline1(text: R.strings.addAccount),
                   Padding(
                     padding: const EdgeInsets.all(32),
-                    child: Form(
-                      child: Column(
-                        children: <Widget>[
-                          NameInput(),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 8.0),
-                            child: EmailInput(),
-                          ),
-                          PasswordInput(),
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(top: 8.0, bottom: 32.0),
-                            child: PasswordConfirmationInput(),
-                          ),
-                          SignUpButton(),
-                          FlatButton.icon(
-                            onPressed: () {},
-                            icon: Icon(Icons.exit_to_app),
-                            label: Text(R.strings.login),
-                          ),
-                        ],
+                    child: Provider(
+                      create: (_) => presenter,
+                      child: Form(
+                        child: Column(
+                          children: <Widget>[
+                            NameInput(),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 8.0),
+                              child: EmailInput(),
+                            ),
+                            PasswordInput(),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 8.0, bottom: 32.0),
+                              child: PasswordConfirmationInput(),
+                            ),
+                            SignUpButton(),
+                            FlatButton.icon(
+                              onPressed: () {},
+                              icon: Icon(Icons.exit_to_app),
+                              label: Text(R.strings.login),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
