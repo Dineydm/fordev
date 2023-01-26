c34a37431748a7e96146d8a98288e6187b887e9c
Diney <dineydm@gmail.com>
2023-01-26 11:03:10 -0300

    ensure RemoteLoadSurveys throws UnexpectedError on 500

--- test/data/usecases/load_surveys/remote_load_surveys_test.dart
+++ test/data/usecases/load_surveys/remote_load_surveys_test.dart
@@ -116,4 +116,12 @@ void main() {
 
     expect(future, throwsA(DomainError.unexpected));
   });
+
+  test('Should throw UnexpectedError if HttpClient returns 500', () {
+    mockHttpError(HttpError.serverError);
+
+    final future = sut.load();
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
