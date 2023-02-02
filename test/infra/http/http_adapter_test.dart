6ea043b7c8ade3352e2195d9167a981d4e6b38c2
Diney <dineydm@gmail.com>
2023-02-02 10:45:27 -0300

    add headers to HttpClient

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -51,12 +51,27 @@ void main() {
         method: 'post',
         body: {'any_key': 'any_value'},
       );
-
       verify(client.post(
         url,
         headers: {
           'content-type': 'application/json',
           'accept': 'application/json'
+        },
+        body: '{"any_key":"any_value"}',
+      ));
+
+      await sut.request(
+        url: url,
+        method: 'post',
+        body: {'any_key': 'any_value'},
+        headers: {'any_header': 'any_value'},
+      );
+      verify(client.post(
+        url,
+        headers: {
+          'content-type': 'application/json',
+          'accept': 'application/json',
+          'any_header': 'any_value',
         },
         body: '{"any_key":"any_value"}',
       ));
@@ -177,7 +192,6 @@ void main() {
 
     test('Should call get with correct values', () async {
       await sut.request(url: url, method: 'get');
-
       verify(client.get(
         url,
         headers: {
@@ -185,6 +199,17 @@ void main() {
           'accept': 'application/json',
         },
       ));
+
+      await sut.request(
+        url: url,
+        method: 'get',
+        headers: {'any_header': 'any_value'},
+      );
+      verify(client.get(url, headers: {
+        'content-type': 'application/json',
+        'accept': 'application/json',
+        'any_header': 'any_value',
+      }));
     });
 
     test('Should return data if get returns 200', () async {
